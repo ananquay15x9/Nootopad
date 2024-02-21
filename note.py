52efa9aa0297acba9a75ddeff93a6aeace72bc03
Tu Le <ttlvt4@umsystem.edu>
2024-02-20 18:55:05 -0600

    first commit

---
+++ note.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+db = SQLAlchemy(app)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+
+@app.route('/')
+def index():
+    notes = Note.query.all()
+    return render_template('index.html', notes=notes)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_note = Note(title=title, content=content)
+        db.session.add(new_note)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('create.html')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    note = Note.query.get_or_404(id)
+    if request.method == 'POST':
+        note.title = request.form['title']
+        note.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', note=note)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    note = Note.query.get_or_404(id)
+    db.session.delete(note)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+    
+
---
+++ static/js/script.js
@@ -0,0 +1,4 @@
+function redirectToEditPage(editUrl) {
+    // Redirect the user to the "Edit" page using the provided URL
+    window.location.href = editUrl;
+}
